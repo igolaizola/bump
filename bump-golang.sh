208f1ea64a9f74a43ff8605fa1203020ed7b0223
igolaizola <11333576+igolaizola@users.noreply.github.com>
2021-12-15 11:17:19 +0100

    Update golang urls

--- bump-golang.sh
+++ bump-golang.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-LATEST=$(curl -s https://golang.org/dl/ | grep ".linux-amd64.tar.gz</span>" | sed -e 's/.*go\(.*\).linux.*/\1/')
+LATEST=$(curl -s https://go.dev/dl/ | grep ".linux-amd64.tar.gz</span>" | sed -e 's/.*go\(.*\).linux.*/\1/')
 CURRENT=$(go version |  sed -e 's/.*go\(.*\) linux.*/\1/')
 if [ $LATEST = $CURRENT ]; then
     echo Golang version is up to date: $LATEST
 else
     echo Updating golang from $CURRENT to $LATEST
-    wget https://dl.google.com/go/go$LATEST.linux-amd64.tar.gz
+    wget https://go.dev/dl/go$LATEST.linux-amd64.tar.gz
     sudo rm -rf /usr/local/go
     sudo tar -C /usr/local -xzf go$LATEST.linux-amd64.tar.gz
     rm go$LATEST.linux-amd64.tar.gz
