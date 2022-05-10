e06691515fe454e582018401b06f8b267c6aec53
igolaizola <11333576+igolaizola@users.noreply.github.com>
2022-05-10 11:38:14 +0200

    Add quotes to pass shellcheck warnings

--- bump-golang.sh
+++ bump-golang.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 LATEST=$(curl -s https://go.dev/dl/ | grep ".linux-amd64.tar.gz</span>" | sed -e 's/.*go\(.*\).linux.*/\1/')
 CURRENT=$(go version |  sed -e 's/.*go\(.*\) linux.*/\1/')
-if [ $LATEST = $CURRENT ]; then
-    echo Golang version is up to date: $LATEST
+if [ "$LATEST" = "$CURRENT" ]; then
+    echo Golang version is up to date: "$LATEST"
 else
-    echo Updating golang from $CURRENT to $LATEST
-    wget https://go.dev/dl/go$LATEST.linux-amd64.tar.gz
+    echo Updating golang from "$CURRENT" to "$LATEST"
+    wget "https://go.dev/dl/go$LATEST.linux-amd64.tar.gz"
     sudo rm -rf /usr/local/go
-    sudo tar -C /usr/local -xzf go$LATEST.linux-amd64.tar.gz
-    rm go$LATEST.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xzf "go$LATEST.linux-amd64.tar.gz"
+    rm "go$LATEST.linux-amd64.tar.gz"
 fi
